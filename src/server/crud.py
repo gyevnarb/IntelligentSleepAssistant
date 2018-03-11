d1c3636d752d769144d51b6ff96ba44bfe72fffc
Frederick Bawden <frederickbawden@Fredericks-MacBook-Air.local>
2018-03-11 10:43:05 +0000

    Added POST and GET for processed data

--- src/server/crud.py
+++ src/server/crud.py
@@ -8,8 +8,6 @@ import os
 import datetime
 import myFitBit
 
-
-
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'crud.sqlite')
@@ -19,8 +17,24 @@ ma = Marshmallow(app)
 class MyEncoder(JSONEncoder):
     def default(self, o):
         return o.__dict__
-
-
+class ProcessedData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tempData = db.Column(db.String(80))
+    lightData = db.Column(db.String(80))
+    moistureData = db.Column(db.String(80))
+    airData = db.Column(db.String(80))
+    def __init__(self,data):
+        self.tempData = str(data[0])
+        self.lightData = str(data[1])
+        self.moistureData = str(data[2])
+        self.airData = str(data[3])
+    def serialize(self):
+        return {
+            'tempData' : self.tempData,
+            'lightData' : self.lightData,
+            'moistureData' : self.moistureData,
+            'airData' : self.airData
+        }
 
 class MBedData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -50,14 +64,54 @@ class UserSchema(ma.Schema):
     class Meta:
         # Fields to expose
         fields = ('date', 'time', 'tempData', 'lightData', 'moistureData','airData');
-
+class ProcessSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ('tempData', 'lightData', 'moistureData','airData');
 
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
 
+process_schema = ProcessSchema()
+process_schema = ProcessSchema(many=False)
+
+
 @app.route("/")
 def hello():
     return "Hello World!"
+@app.route("/processedData", methods=["POST"])
+def set_processdata():
+    pd = ProcessedData.query.first()
+    temp = str(request.json['Tempr'])
+    light = str(request.json['Light'])
+    co2 = str(request.json['eCO2'])
+    moisture = str(request.json['Humid'])
+    data = [temp,light,moisture,co2]
+    new_entry = ProcessedData(data)
+    db.session.add(new_entry)
+    db.session.commit()
+    return process_schema.jsonify(new_entry)
+
+@app.route("/processedData", methods=["PUT"])
+def update_processdata():
+    pd = ProcessedData.query.first()
+    temp = str(request.json['Tempr'])
+    light = str(request.json['Light'])
+    co2 = str(request.json['eCO2'])
+    moisture = str(request.json['Humid'])
+    pd.temp = temp
+    pd.light = light
+    pd.co2 = co2
+    pd.moisture = moisture
+    db.session.commit()
+    return process_schema.jsonify(pd)
+
+@app.route("/processedData", methods=["GET"])
+def get_processdata():
+    first_data = ProcessedData.query.all()
+    result = process_schema.dump(first_data[-1]);
+    return jsonify(result.data)
+
 # endpoint to create new user
 @app.route("/data", methods=["POST"])
 def add_user():
@@ -71,7 +125,6 @@ def add_user():
     light = str(request.json['Light'])
     co2 = str(request.json['eCO2'])
     moisture = str(request.json['Humid'])
-    #data = []
     data = [temp,light,moisture,co2]
     new_entry = MBedData(date ,time, data)
     db.session.add(new_entry)
